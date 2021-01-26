ad3f1b2e3bb7101ac49fc7eefbacfb93e18b1db6
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-01-26 10:14:13 +0100

    tool is READY!!!

---
+++ README.md
@@ -0,0 +1,22 @@
+# Intro
+
+A simple CLI tool that helps to check if a package name is available for use on npm registry
+
+# Installation
+`- git clone/download repo`
+`- run 'npm install' command in project directory`
+`- start tool with 'node app.js' command`
+
+# Usage
+
+```
+This CLI tool is pretty easy and simple to use
+BUT....
+if you see anything that looks like:
+    'connect ETIMEDOUT'
+    it signifies connection timeout and the check was not completed successfully
+But if it is something like:
+    'Request failed with status code 404'
+    Then that's a good news, package name is available!
+
+```
---
+++ app.js
@@ -0,0 +1,79 @@
+const wrapper = require("simple-api-wrapper");
+const prompt = require("prompt");
+const colors = require("colors/safe");
+
+prompt.start();
+
+const intro = () => {
+    console.log(
+        colors.green(
+            `
+            ***************************************************************************************************************\n
+            ***************************************************************************************************************\n
+            This CLI tool is pretty easy and simple to use\n
+            BUT....\n
+            if you see anything that looks like:\n
+            'connect ETIMEDOUT'\n
+            it signifies connection timeout and the check was not completed successfully\n
+            But if it is something like:\n
+            'Request failed with status code 404'\n
+            Then that's a good news, package name is available!\n
+            ***************************************************************************************************************\n
+            ***************************************************************************************************************\n\n\n\n\n\n\n
+            `
+        )
+    );
+}
+
+const handleError = (error) => {
+    console.log(error.message);
+    return 1;
+}
+
+const retry = () => {
+    console.log(colors.yellow.underline("Try again?"));
+    prompt.get(['restart'], (err, result) => {
+        if (err) { return handleError(error); }
+        // console.log(colors.green('Input received . . .'));
+        if (result.restart.toLowerCase().includes("y"))
+            startTool();
+        else
+            console.log(colors.red.bold("Shutting down . . ."));
+    });
+}
+
+const checkName = (name) => {
+    //endpoints and data
+    const config = {
+            get: [
+                    {
+                        [`https://registry.npmjs.org/${name}`]: {},
+                    }
+                ],
+            };
+
+    const allResponse = wrapper.apiWrapper(config);
+    allResponse.then(result => {
+        if (result) {
+            console.log(colors.green(`${colors.red.bold.underline(name)} cannot be used as node package name cos it already exist`));
+            retry();
+        }
+        else {
+            console.log(colors.green(`${name} might still available, ${colors.red.bold.underline('npm publish')} that package ${colors.bold('NOW ! ! ! ')}`))
+            retry();
+        }
+    });
+}
+
+const startTool = () => {
+    console.log(colors.green("Welcome!\nPlease enter the name you want to check"));
+    prompt.get(['packageName'], (err, result) => {
+        if (err) { return handleError(error); }
+        // console.log(colors.green('Input received . . .'));
+        console.log(colors.green(`Checking for the availability of ${colors.yellow.bold.underline(result.packageName)} . . .`));
+        checkName(result.packageName);
+    });
+}
+
+intro();
+startTool();
